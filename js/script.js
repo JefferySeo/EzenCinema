dbc7e47ce8a62a41c1b25bfb45ccfcf7611800a9
JefferySeo <125331815+JefferySeo@users.noreply.github.com>
2023-05-07 01:17:19 +0900

    슬라이드 작업중

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(function(){
+    // header 내비게이션 작동
+    $('.header-gnb>li:not(:nth-child(5))>a').mouseover(function(){
+        $('.header-hide').removeClass('on');
+        $('.header-gnb>li>a').removeClass('on')
+        $('.header-gnb>li>.header-lnb').removeClass('on');
+        $('.header-hide').addClass('on');
+        $(this).addClass('on')
+        $(this).next('.header-lnb').addClass('on');
+    })
+    $('.header-hide').mouseleave(function(){
+        $('.header-hide').removeClass('on');
+        $('.header-gnb>li>a').removeClass('on')
+        $('.header-gnb>li>.header-lnb').removeClass('on');
+    })
+
+    // hero 슬라이드
+    setInterval(mySlide,3000);
+    function mySlide(){
+        $('.ts-slide-box').animate({
+            'left': "-1100px"
+        },500, function(){
+            $('.ts-slide-box a:first-child')
+            .clone()
+            .appendTo('.ts-slide-box');
+            $('.ts-slide-box a:first-child').remove();
+            $('.ts-slide-box').css('left',0);
+        });
+    }
+})
