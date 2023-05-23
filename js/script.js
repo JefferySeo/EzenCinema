da47bd44156bc00ebcbdc3ad3626f26059b698af
JefferySeo <125331815+JefferySeo@users.noreply.github.com>
2023-05-23 17:57:34 +0900

    ss

--- js/script.js
+++ js/script.js
@@ -1,39 +1,39 @@
 $(function(){
     // header 내비게이션 작동
-    $('.header-gnb>li:not(:nth-child(5))>a').mouseover(function(){
-        $('.header-hide').removeClass('on');
-        $('.header-gnb>li>a').removeClass('on')
-        $('.header-gnb>li>.header-lnb').removeClass('on');
-        $('.header-hide').addClass('on');
-        $(this).addClass('on')
-        $(this).next('.header-lnb').addClass('on');
+    $(".gnb>li").hover(function(){
+        $(this).find(".lnb").stop().fadeToggle(300);
     })
-    $('.header-hide').mouseleave(function(){
-        $('.header-hide').removeClass('on');
-        $('.header-gnb>li>a').removeClass('on')
-        $('.header-gnb>li>.header-lnb').removeClass('on');
-    })
-
-    // hero 슬라이드
-    let heroInterval = setInterval(mySlide,3000);
-    function mySlide(){
-        $('.ts-slide-box').animate({
-            'left': "-1100px"
-        },500, function(){
-            $('.ts-slide-box .slide:first-child')
-            .clone()
-            .appendTo('.ts-slide-box');
-            $('.ts-slide-box .slide:first-child').remove();
-            $('.ts-slide-box').css('left',0);
+    $(document).ready(function(){
+        // header-search 나와라
+        $("#search-on").click(function(event){
+            event.stopPropagation();
+            $(".header-search-box").fadeToggle(200);
         });
-    }
-    $('.ts-slide-frame').mouseover(function(){
-        clearInterval(heroInterval);
-    }).mouseleave(function(){
-        heroInterval = setInterval(mySlide, 3000);
-    });
-
-    $('.office-moviebox').hover(function(){
-        $(this).find('.office-review').stop().fadeToggle(200);
+        // header-search 다른데 눌러도 없어져라
+        $(document).click(function(event){
+            if ($(".header-search-box").is(":visible") && !$(event.target).closest(".header-search-box").length) { // 서치폼이 보이는 상태인 경우에만 처리
+                $(".header-search-box").fadeOut(200);
+            }
+        })
     })
+    
+    // 스크롤하면 header 변신
+    $(window).scroll(function(){
+        const header = $('#header').offset().top;
+        if(header > 0){
+            $('#header').css({
+                position:'fixed',
+                top:0,
+                border:'none'
+            })
+        }else{
+            $('#header').css({
+                position:'relative',
+                top:'30px',
+                border:'1.8px solid #000'
+            })
+        }
+    })
+    
+    
 })
