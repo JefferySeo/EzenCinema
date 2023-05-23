85bebb41088d459b52600e92c9dbd23f338f785f
bk4533 <125331844+bk4533@users.noreply.github.com>
2023-05-24 00:06:41 +0900

    add
    add

--- js/script.js
+++ js/script.js
@@ -14,8 +14,30 @@ $(function(){
         $('.header-gnb>li>.header-lnb').removeClass('on');
     })
     // 영화상세
+});
 $(function(){
+        //좋아요 버튼
+        $(document).ready(function() {
+            $(".k_mv_datat_like").click(function() {
+              $("#likeimage").attr("src", function(index, currentSrc) {
+                if (currentSrc === "images/moviedetail/likeoff.png") {
+                  return "images/moviedetail/likeon.png";
+                } else {
+                  return "images/moviedetail/likeoff.png";
+                }
+              });
+            });
+          });
+        //좋아요 버튼
+        // $('#likeimage').click(function(){
+        //     $('#likeimage').toggleClass()
+        // })
 
+        // likeimage라는 이미지를 클릭했을때
+        // 이미지 소스가 src="images/moviedetail/likeon.png"으로
+        // 변경되고 누르때마다 on off으로 변경된다
+        
+        //정보 리뷰 active
         $('.k-tab>li').click(function(){
             $('.k-tab>li>a').toggleClass('active');
             if($('#information').hasClass("active")){
@@ -29,21 +51,32 @@ $(function(){
                 $('#k-review').removeClass("active");
             }
         });
-        // $(document).ready(function(){
-            
-        // });
-        
+
         $(document).ready(function() {
-            $('fieldset.rate input').on('click', function() {
+            
+        //textarea 글자입력 설정
+        $('fieldset.rate input').on('click', function() {
               var ratingValue = $(this).val();
               $('.rating-number').text(ratingValue);
             });
-          });
+        });
+
+        //textarea 글자입력 설정
+        $('.k-text_box textarea').keyup(function(){
+            var content = $(this).val();
+            $('.k-text_box .count span').html(content.length);
+            if (content.length > 200){
+              alert("최대 220자까지 입력 가능합니다.");
+              $(this).val(content.substring(0, 220));
+              $('.k-text_box .count span').html(220);
+            }
+        });
 });
 
 
-});
+
 $(function(){
+
     // 스토어메뉴 fixed
     $(window).scroll(function(){
         const storemenu = $('#k-menu').offset().top;
