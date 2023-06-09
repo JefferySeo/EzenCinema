2256ef8ff7c8383210ac4d4b613e9df3209e004f
bohyeon0103 <kbh2753@gmail.com>
2023-06-10 00:54:41 +0900

    그냥저냥~

--- js/script.js
+++ js/script.js
@@ -1,7 +1,28 @@
 $(function () {
+    $('.snack').hide();
+    $('.drink').hide();
+
     $('.tab li').click(function () {
         $('.tab li').removeClass('on')
         $(this).addClass('on')
+
+        let num = $(this).index()
+        console.log(num)
+
+
+        if (num == 0) {
+            $('.snack').hide();
+            $('.drink').hide();
+            $('.combo').show();
+        } else if (num == 1) {
+            $('.combo').hide();
+            $('.drink').hide();
+            $('.snack').show();
+        } else if (num == 2) {
+            $('.combo').hide();
+            $('.snack').hide();
+            $('.drink').show();
+        }
     });
 
 
